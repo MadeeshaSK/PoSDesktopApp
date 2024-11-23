14a2faff08e3f1c8cad53104d765728eee424f20
MadeeshaSK <madeeshasachindu2@gmail.com>
2024-11-23 15:56:10 +0530

    first-commit

---
+++ src/main/java/com/mycompany/posapp/PoSApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.posapp;
+
+/**
+ *
+ * @author MADEESHA
+ */
+public class PoSApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
